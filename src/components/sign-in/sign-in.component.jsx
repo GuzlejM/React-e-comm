cece69e5402d93d4712ebf95c17bbde1b807fd73
GuzlejM <martin.guzlej@gmail.com>
2022-07-27 17:44:31 +0200

    added Login func, styling button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import axios from "axios";
 import GoogleLogin from "react-google-login";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
@@ -16,20 +16,51 @@ const SignIn = () => {
   const [formFields, setFormFields] = useState(defaultFormValues);
   const { email, password } = formFields;
 
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
+
   const handleChange = (event) => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name]: value });
   };
 
-  const handleSubmit = async (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
 
     try {
-      await resetFormFields();
+      const url = "http://localhost:5000/api/auth/login";
+      const data = await axios.post(url, formFields);
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
+
+    await resetFormFields();
+  };
+
+  const handleLoginGoogle = async (googleData) => {
+    const res = await fetch("api/google-login", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem("loginData", JSON.stringify(data));
+  };
+
+  const handleLogoutGoogle = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
   };
 
   const resetFormFields = () => {
@@ -41,7 +72,7 @@ const SignIn = () => {
       <h2>I already have an account</h2>
       <span>Sign in with your email and password</span>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleLogin}>
         <FormInput
           name="email"
           handleChange={handleChange}
@@ -59,13 +90,25 @@ const SignIn = () => {
           required
         />
 
-        <CustomButton type="submit">Sign in</CustomButton>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText="Log in with Google"
-          onSuccess={handleSubmit}
-          cookiePolicy={"single_host_origin"}
-        ></GoogleLogin>
+        <div className="buttons-container">
+          <CustomButton type="submit">Sign in</CustomButton>
+
+          <div>
+            {loginData ? (
+              <div>
+                <h3>You logged in as {loginData.email}</h3>
+                <button onClick={handleLogoutGoogle}></button>
+              </div>
+            ) : (
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Log in with Google"
+                onSuccess={handleLoginGoogle}
+                cookiePolicy={"single_host_origin"}
+              ></GoogleLogin>
+            )}
+          </div>
+        </div>
       </form>
     </div>
   );
